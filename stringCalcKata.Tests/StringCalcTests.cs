7394e48d28e27366a669bf6669b050ad3a1d2704
korfu92 <konrad.korf@gmail.com>
2018-09-01 10:43:28 +0200

    6th test

--- stringCalcKata.Tests/StringCalcTests.cs
+++ stringCalcKata.Tests/StringCalcTests.cs
@@ -57,5 +57,30 @@ namespace stringCalcKata.Tests
             //assert
             Assert.AreEqual(3, result);
         }
+        [Test]
+        public void Add_threeNumbers_returnsTheirSum()
+        {
+            //arrange
+            var sut = new StringCalc();        //SUT - system under tests
+
+            //act
+            var result = sut.Add("25,1452,22");
+
+            //assert
+            Assert.AreEqual(1499, result);
+        }
+        [Test]
+        public void Add_fourNumbersDividedByComma_returnsTheirSum()
+        {
+            //arrange
+            var sut = new StringCalc();        //SUT - system under tests
+
+            //act
+            var result = sut.Add("25,1452,22,134234234");
+
+            //assert
+            Assert.AreEqual(134235733, result);
+        }
+
     }
 }
